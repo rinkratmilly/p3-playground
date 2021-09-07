1fb21be2b359f3307e339215f2493c6764960c2f
softeng2021 <naeemgibson@gmail.com>
2021-09-07 00:49:55 -0400

    proposed create and show routes

--- Backend/models/nft.js
+++ Backend/models/nft.js
@@ -6,15 +6,21 @@ const  NFTSchema = new Schema({
     token_id: String,
     background_color: String,
     image_url: String,
-    image_preview_url: String,
-    image_thumbnail_url: String,
-    image_original_url: String,
     animation_url: String,
     animation_original_url: String,
     name: String,
     description: String,
     external_link: String,
-    permalink:String
+    permalink:String,
+    banner_image_url: String,
+    created_date: String,
+    dev_buyer_fee_basis_points: String,
+    dev_seller_fee_basis_points: String,
+    opensea_buyer_fee_basis_points: String,
+    opensea_seller_fee_basis_points: String,
+    payout_address: String,
+    slug: String,
+    twitter_username: String
 })
 
 
